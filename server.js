16d83d70b689bed3ef5a7610f13e5833b6e446f6
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-08-14 10:21:33 -0500

    add express.js middleware and PORT 3001 function

--- server.js
+++ server.js
@@ -4,3 +4,22 @@ const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database 
+const database = mysql.createConnection({
+        host: 'localhost',
+        port: '3001',
+        user: 'root',
+        password: 'Malcolm_01',
+        database: 'departments'
+    },
+    console.log('connected')
+);
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
