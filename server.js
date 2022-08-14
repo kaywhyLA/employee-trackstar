2c62d3028881ee90cd019c06cedd3e237114fcdb
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-08-14 10:33:42 -0500

    created employeeUpdate prompt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,6 +20,52 @@ const database = mysql.createConnection({
     },
     console.log('connected')
 );
+
+// first prompt question for employer choice
+const employeeUpdate = () => {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'employeeChoice',
+        message: 'What would you like to do?',
+        choices: [
+            'View all Employees',
+            'View Departments',
+            'View Roles',
+            'Add Employee',
+            'Update Employee Role',
+            'Exit'
+        ]
+    }]).then((answer) => {
+        switch (answer.employeeChoice) {
+            case 'View all Employees':
+                viewAllEmployees();
+                break;
+
+            case 'View Departments':
+                viewDepartments();
+                break;
+
+            case 'View Roles':
+                viewRoles();
+                break;
+
+            case 'Add Employee':
+                addEmployee();
+                break;
+
+            case 'Add Departments':
+                addDepartment();
+                break;
+            case 'Add Roles':
+                addRoles();
+                break;
+
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+        }
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
