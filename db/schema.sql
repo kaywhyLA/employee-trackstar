85741e26d6cb653752080cffa07ec055f2a4532a
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-08-14 11:55:50 -0500

    created employee table in mysql then copied over to seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,20 @@
+--database for employeeTracker
+DROP DATABASE IF EXISTS employeeTracker;
+CREATE DATABASE employeeTracker;
+USE employeeTracker;
+
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
+    PRIMARY KEY (id)
+);
+
+-- table for employees
+CREATE TABLE employee (
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30), 
+    last_name VARCHAR(30),
+    role_id INTEGER(11),
+    manager_id INTEGER(11),
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,13 @@ VALUES
 ('Sales'),
 ('Legal'),
 ('Engineering');
+
+INSERT INTO employee (first_name, last_name)
+VALUES 
+('Quentin', 'Tarantino'),
+('Jordan', 'Peele'),
+('Greta', 'Gerwig'),
+('Martin', 'Scorsese'),
+('Wes', 'Anderson'),
+('Christopher', 'Nolan'),
+('Coen', 'Brothers');
